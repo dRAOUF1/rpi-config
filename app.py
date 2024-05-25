6931fe9c016c2ee85d9c380adca495a709ff6b46
dRAOUF1 <119814672+dRAOUF1@users.noreply.github.com>
2024-05-25 14:04:22 +0100

    test1

--- app.py
+++ app.py
@@ -1,14 +1,25 @@
 from flask import Flask, render_template,request
 import subprocess
 import requests
-import socket
+import socket, os
+from dotenv import load_dotenv
 
 app = Flask(__name__)
-
+load_dotenv()
 @app.route('/')
 def index():
+    default_values = {
+        'mode': os.getenv('MODE', 'reseau'),
+        'room_name': os.getenv('ROOM_NAME', ''),
+        'app_url': os.getenv('URL_BACKEND', ''),
+        'rotation': os.getenv('ROTATION', '0'),
+        'detection_threshold': os.getenv('DETECTION_THRESHOLD', '0.65'),
+        'recognition_threshold': os.getenv('RECOGNITION_THRESHOLD', '0.45'),
+        'tracker_max_distance': os.getenv('TRACKER_MAX_DISTANCE', '90'),
+        'tracker_max_frame_loss': os.getenv('TRACKER_MAX_FRAME_LOSS', '4')
+    }
     # Rendre le fichier HTML index.html
-    return render_template('./index.html')
+    return render_template('index.html', default_values=default_values)
 
 
 
@@ -20,32 +31,44 @@ def connexion():
     password = request.form['password']
     appURL = request.form['appURL']
     rotation = request.form['rotation']
-    print(f"roomName: {roomName}, ssid: {ssid}, password: {password}, appURL: {appURL}, rotation: {rotation}")
+    detection_threshold = request.form['detection_threshold']
+    recognition_threshold = request.form['recognition_threshold']
+    tracker_max_distance = request.form['tracker_max_distance']
+    tracker_max_frame_loss = request.form['tracker_max_frame_loss']
+    mode = request.form["mode"]
+    print(f"roomName: {roomName}, ssid: {ssid}, password: {password}, appURL: {appURL}, rotation: {rotation}, detection_threshold: {detection_threshold}, recognition_threshold: {recognition_threshold}, tracker_max_distance: {tracker_max_distance}, tracker_max_frame_loss: {tracker_max_frame_loss}")
     hostname = socket.gethostname()
     IPAddr = socket.gethostbyname(hostname)
     
-    r = requests.post(f"{appURL}/addRaspberryPi",json={"salle":roomName,"addressIp":IPAddr})
-    # Execute the command
-    process = subprocess.Popen('nmcli con delete localnet', shell=True, stdout=subprocess.PIPE, stderr=subprocess.PIPE)
+    if mode=="reseau":
+        #r = requests.post(f"{appURL}/addRaspberryPi",json={"salle":roomName,"addressIp":IPAddr})
+        # Execute the command
+        process = subprocess.Popen('nmcli con delete localnet', shell=True, stdout=subprocess.PIPE, stderr=subprocess.PIPE)
 
-    # Wait for the command to finish and get the return code
-    return_code = process.wait()
+        # Wait for the command to finish and get the return code
+        return_code = process.wait()
 
-    process = subprocess.Popen(f"sudo nmcli con add type wifi ifname 'wlan0' ssid '{ssid}' \
-    con-name 'localnet' -- wifi-sec.key-mgmt 'wpa-psk' \
-    wifi-sec.psk '{password}'", shell=True, stdout=subprocess.PIPE, stderr=subprocess.PIPE)
+        process = subprocess.Popen(f"sudo nmcli con add type wifi ifname 'wlan0' ssid '{ssid}' \
+        con-name 'localnet' -- wifi-sec.key-mgmt 'wpa-psk' \
+        wifi-sec.psk '{password}'", shell=True, stdout=subprocess.PIPE, stderr=subprocess.PIPE)
+        
+        return_code = process.wait()
+        # if return_code != 0:
+        #     return render_template('./erreur.html',erreur="Erreur lors de la création de la connexion au réseau wifi")
     
-    return_code = process.wait()
-    if return_code != 0:
-        return render_template('./erreur.html',erreur="Erreur lors de la création de la connexion au réseau wifi")
+
+    
     
     try:
         with open('/home/pi/Desktop/PFE/.env','w') as f:
-            f.write(f"ROOM_NAME={roomName}\nURL_BACKEND={appURL}\nROTATION={rotation}")
+            f.write(f"MODE={mode}\nROOM_NAME={roomName}\nURL_BACKEND={appURL}\nROTATION={rotation}\n DETECTION_THRESHOLD={detection_threshold/100}\nRECOGNITION_THRESHOLD={recognition_threshold}\nTRACKER_MAX_DISTANCE={tracker_max_distance}\nTRACKER_MAX_FRAME_LOSS={tracker_max_frame_loss}")
+        with open('.env','w') as f:
+            f.write(f"MODE={mode}\nROOM_NAME={roomName}\nURL_BACKEND={appURL}\nROTATION={rotation}\n DETECTION_THRESHOLD={detection_threshold/100}\nRECOGNITION_THRESHOLD={recognition_threshold}\nTRACKER_MAX_DISTANCE={tracker_max_distance}\nTRACKER_MAX_FRAME_LOSS={tracker_max_frame_loss}")
     except FileNotFoundError:
         return render_template('./erreur.html',erreur="Fichier de configuration introuvable")
 
-    process = subprocess.Popen('sleep 10 && sudo reboot', shell=True, stdout=subprocess.PIPE, stderr=subprocess.PIPE)
+    if mode=="reseau":
+        process = subprocess.Popen('sleep 10 && sudo reboot', shell=True, stdout=subprocess.PIPE, stderr=subprocess.PIPE)
     return render_template('./configurationTermine.html')
     
 
